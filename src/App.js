3dab5559ce706aea9f6ef773b8ec2a7308575b37
DKYoungOne <bawsinator@gmail.com>
2023-05-17 11:04:13 -0500

    Add todo functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isClicked: false,
+      todos: ['what', 'to', 'do'],
+      text: ''
+    }
+  }
+  
+  onClickHandler = () => {
+    console.log(this.state)
+    this.setState(
+      {
+        isClicked: !this.state.isClicked
+        
+      }
+    )
+  }
+
+  onAddTodo = () => {
+    this.setState({
+      todos: [...this.state.todos, this.state.text],
+  
+    })
+    console.log(this.state);
+    
+  }
+
+  onChangeHandler = ({ target }) => {
+    console.log(target.value);
+    this.setState({
+      text: target.value,
+    })
+  }
+  
+  handleDelete = idx => {
+    this.setState({
+      todos: this.state.todos.filter((todo, i) => i !== idx)
+    })
+  }
+
+  render() {
+      return (
+        <div className="App">
+            <button type="button" onClick={this.onClickHandler}>
+              {this.state.isClicked ? 'It is clicked' : 'Not clicked'}
+            </button>
+            <br />
+            <input type="text" onChange={this.onChangeHandler}></input>
+            <button type="button" onClick={this.onAddTodo}>Add Todo</button>
+            
+            <ul>
+              {this.state.todos.map((todo, idx) => {
+                return (
+                  <li key={idx}>
+                    {todo}{' '}
+                    <button type="button" onClick={() => this.handleDelete(idx)}>Delete</button>
+                  </li>
+                )
+                  
+              })}
+            </ul>
+            
+        </div>
+    );
+  }
 }
 
 export default App;
